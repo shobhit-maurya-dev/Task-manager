b55f6770781cf50428abb42a2806f25a0ea6370e
Your Ankitkumar341 <gamerhumai.1@gmail.com>
2026-04-07 01:01:10 +0530

    chore(frontend): move firebase keys to environments

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,4 +1,12 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api'
+  apiUrl: 'http://localhost:8080/api',
+  firebaseConfig: {
+    apiKey: 'AIzaSyACmkGK62YPl-Lbm1629aRRanH2Ta69-b0',
+    authDomain: 'govalert-1jsnb.firebaseapp.com',
+    projectId: 'govalert-1jsnb',
+    storageBucket: 'govalert-1jsnb.firebasestorage.app',
+    messagingSenderId: '793240282730',
+    appId: '1:793240282730:web:53e3308c355d7c42088c13'
+  }
 };
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,11 +2,11 @@ export const environment = {
   production: true,
   apiUrl: 'https://taskflow-backend-9o23.onrender.com/api',
   firebaseConfig: {
-    apiKey: 'FIREBASE_API_KEY_PLACEHOLDER',
+    apiKey: 'AIzaSyACmkGK62YPl-Lbm1629aRRanH2Ta69-b0',
     authDomain: 'govalert-1jsnb.firebaseapp.com',
     projectId: 'govalert-1jsnb',
     storageBucket: 'govalert-1jsnb.firebasestorage.app',
     messagingSenderId: '793240282730',
-    appId: 'FIREBASE_APP_ID_PLACEHOLDER'
+    appId: '1:793240282730:web:53e3308c355d7c42088c13'
   }
 };
